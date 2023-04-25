ac9992198bbfa6c03d03a6daaacbaa698d336b1d
Martin Ducoulombier <martin.ducoulombier@free.fr>
2023-04-25 18:46:46 +0200

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -5,6 +5,22 @@
 
     <div id="footer">
 
+        <div id="nousContacter">
+            <div class="contactTitre">Nous contacter</div>
+            <div class="contactContent">chatsparminou@gmail.com</div>
+            <div class="contactContent">06 06 06 06 06</div>
+            <div class="contactContent">Adresse du refuge</div>
+        </div>
+        <div id="reseauxSociaux">
+            <div class="contactTitre">Réseaux sociaux</div>
+            <a class="contactContent">Facebook</a>
+            <a class="contactContent">Instagram</a>
+            <a class="contactContent">Twitter</a>
+        </div>
+        <div id="lienImportant">
+            <div class="contactTitre">Lien important</div>
+        </div>
+
     </div>
 
 
