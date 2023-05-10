8011be412624542f4f9bd377181c36f7ffd92b18
Maxche124 <you@example.com>
2023-05-11 01:21:34 +0200

    update style qui sommes nous

--- templates/quiSommesNous.php
+++ templates/quiSommesNous.php
@@ -13,7 +13,7 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php"){ // Si la page est appelée d
 
 <div>
 	<div class="qsn">
-		<a target="_blank" href="ressources/chats/0_0.jpg">
+		<a target="_blank" href="ressources/chats/3/0.jpg">
 			<img src="ressources/chats/3/0.jpg" alt="poti chat">
 		</a>
 		<div class="qsnContent">
@@ -31,13 +31,13 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php"){ // Si la page est appelée d
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
 		</div>
 		</div>
-		<a target="_blank" href="ressources/chats/0_0.jpg">
+		<a target="_blank" href="ressources/chats/2/0.jpg">
 			<img src="ressources/chats/2/0.jpg" alt="poti chat">
 		</a>
 	</div>
 
 	<div class="qsn">
-		<a target="_blank" href="ressources/chats/0_0.jpg">
+		<a target="_blank" href="ressources/chats/1/0.jpg">
 			<img src="ressources/chats/1/0.jpg" alt="poti chat">
 		</a>
 		<div class="qsnContent qsnTexte">
@@ -51,17 +51,17 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php"){ // Si la page est appelée d
 	<div class="qsn policeTexte">
 		<div class="qsnStats qsnTexte">
 			<h1>500</h1>
-			<p>chats pris en charge</p>
+			<p>Chats pris en charge</p>
 		</div>
 
 		<div class="qsnStats qsnTexte">
 			<h1>25</h1>
-			<p>chats adoptés depuis le début d'année</p>
+			<p>Chats adoptés depuis le début d'année</p>
 		</div>
 
 		<div class="qsnStats qsnTexte">
 			<h1>18</h1>
-			<p>bénévoles</p>
+			<p>Bénévoles</p>
 		</div>
 	</div>
 
