35087f5022ee25db933bdf8c6c1bd49a247908be
Hugz12 <hugolaloy@icloud.com>
2023-06-18 20:32:49 +0200

    Oui

--- js/conseilsEtViePratique.js
+++ js/conseilsEtViePratique.js
@@ -1,7 +1,122 @@
-// URI: https://www.lafermeduweb.net/bibliotheque-javascript/conseils-et-vie-pratique-9.html
-// Description: Conseils et vie pratique
-// Version: 1.0.0
+var width = window.innerWidth;
+        
+function resize() {
+    width = window.innerWidth;
 
-function afficherConseils(conseils){
-    console.log(conseils);
+    svg.attr("width", width).attr("height", height);
+    simulation.force("center", d3.forceCenter(width / 2, height / 2));
+    simulation.restart();
 }
+
+window.addEventListener("resize", resize);
+
+const height = 700;
+const numNodes = 15;
+const radius = 60; // Taille initiale des boules
+const maxScale = 1.7; // Échelle maximale lorsqu'une boule est proche de la souris
+const strength = 1; // Force de répulsion entre les boules
+const collisionRadius = radius * 1.30; // Rayon de collision pour les boules
+
+// Ajoutez vos textes et liens personnalisés ici
+const data = [
+    { text: "OpenAI", link: "https://openai.com" },
+    { text: "Google", link: "https://www.google.com" },
+    { text: "GitHub", link: "https://github.com" },
+    { text: "Wikipedia", link: "https://www.wikipedia.org" },
+    { text: "Facebook", link: "https://www.facebook.com" },
+    { text: "OpenAI", link: "https://openai.com" },
+    { text: "Google", link: "https://www.google.com" },
+    { text: "GitHub", link: "https://github.com" },
+    { text: "Wikipedia", link: "https://www.wikipedia.org" },
+    { text: "Facebook", link: "https://www.facebook.com" },
+    { text: "OpenAI", link: "https://openai.com" },
+    { text: "Google", link: "https://www.google.com" },
+    { text: "GitHub", link: "https://github.com" },
+    { text: "Wikipedia", link: "https://www.wikipedia.org" },
+    { text: "Facebook", link: "https://www.facebook.com" },
+    { text: "OpenAI", link: "https://openai.com" },
+    { text: "Google", link: "https://www.google.com" },
+    { text: "GitHub", link: "https://github.com" },
+    { text: "Wikipedia", link: "https://www.wikipedia.org" },
+];
+
+const nodes = d3.range(numNodes).map((d, i) => {
+    return { radius: radius, scale: 1, x: width / 2, y: height / 2, text: data[i].text, data: data[i] };
+});
+
+const simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(strength))
+    .force("collision", d3.forceCollide().radius(collisionRadius).strength(0.1));
+
+const svg = d3.select("#svg-container")
+    .attr("width", width)
+    .attr("height", height);
+
+let g = svg.append("g"); 
+// fait en sorte que g soient plus large
+
+const circles = g.selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("a") // Ajout de l'élément <a>
+    .attr("xlink:href", d => d.data.link) // Définition du lien
+    .append("circle")
+    .attr("r", d => d.radius)
+    .attr("fill", "#FFC107")
+
+const textLabels = g.selectAll("text")
+    .data(nodes)
+    .enter()
+    .append("a") // Ajout de l'élément <a>
+    .attr("xlink:href", d => d.data.link) // Définition du lien
+    .append("text")
+    .attr("x", d => d.x)
+    .attr("y", d => d.y)
+    .attr("text-anchor", "middle")
+    .attr("dy", "0.3em")
+    .style("font-size", d => d.radius * 0.2)
+    .style("font-family", "Arial")
+    .text(d => d.text);
+
+simulation.on("tick", () => {
+    circles
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", d => d.radius * d.scale);
+
+    textLabels
+        .attr("x", d => d.x)
+        .attr("y", d => d.y + (d.radius * d.scale * 0.1))
+        .style("font-size", d => (d.radius * d.scale * 0.4) + "px");
+});
+
+svg.on("mousemove", (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    nodes.forEach(node => {
+        const dx = node.x - mouseX;
+        const dy = node.y - mouseY;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const maxDistance = radius * 3;
+
+        if (distance < maxDistance) {
+            const scale = 1 + (maxDistance - distance) / maxDistance;
+            node.scale = Math.min(scale, maxScale);
+        } else {
+            node.scale = 1;
+        }
+    });
+
+    simulation.alpha(1).restart();
+});
+
+svg.on("mouseleave", () => {
+    nodes.forEach(node => {
+        node.scale = 1;
+    });
+
+    simulation.alpha(1).restart();
+});
+resize();
+simulation.restart();
--- templates/conseilsEtViePratique.php
+++ templates/conseilsEtViePratique.php
@@ -8,112 +8,12 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php"){ // Si la page est appelée d
 ?>
 
 <script src="./js/utils.js"></script>
-<script src="./js/conseilsEtViePratique.js"></script>
+<script src="https://d3js.org/d3.v7.min.js"></script>
 <link rel="stylesheet" href="css/conseilsEtViePratique.css"> 
 
-<div>
-	<div class="conseilTitre policeTitre">Conseils et vie pratique</div>
-	<div id="conseils">
-		<div id="conteneur-divs"></div>
-		<div class="bulle">
-			<a class="conseilText" href="https://www.youtube.com">
-				Au revoir
-			</a>
-		</div>
-		<div class="bulle">
-			<a class="conseilText" href="https://www.youtube.com">
-				Au revoir
-			</a>
-		</div>
-	</div>
-
-	<script>
-		var conseils = <?= json_encode(getConseils());?>;
-		afficherConseils(conseils);
-	</script>
-
-
-	<?php 
-			if(valider("Admin","SESSION")) {
-
-				echo " 
-					<div class='svgBox'>
-						<div id='svgAdd' class='svg' onclick='displayForm(\"addConseil\")'>
-				";
-							include("ressources/add.svg");
-				echo "
-						</div>
-						<div id='svgDelete' class='svg' onclick='displayForm(\"delConseil\")'>
-				";
-							include("ressources/dustbin.svg");
-				echo "
-						</div>
-					</div>
-				";
-				// inclue le svg dans le html
-				
-			}
-	?>
-	
+<div id="container">
+    <svg id="svg-container"></svg>
 </div>
 
-<?php 
-	if(valider("Admin","SESSION")) {
-		echo "
-			
-			<div id='addEvenement' class='formType' style='display: none'>
+<script src="./js/conseilsEtViePratique.js"></script>
 
-				<div class='buttonHideForm' onclick='hideForm(\"addConseil\");'>
-					<img src='./ressources/fermer_form.png' style='width: 30px; height: 30px;'>
-				</div>
-
-				<div class='policeTitre tailleTitre titreForm'>Ajouter une bulle</div>
-				<br>
-				<form class='policeTexte' action='controleur.php' method='post' enctype='multipart/form-data'>
-					
-
-					<div id='bulleForm'>
-
-						<div class='group'>
-							<input type='text' name='name' required>
-							<label for=\"titre\">Texte</label>
-						</div>
-					
-						<div class='group'>
-							<label for=\"fiche\" class='label-fiche'>Choisir une fiche</label>
-							<input id='fiche' name='value' class='input-fiche' type='file'>
-						</div>
-
-					</div>
-					
-					<input type='submit' class='buttonType' name='action' value='Ajouter Bulle'>
-
-				</form>
-			</div>
-
-
-			<div id='supprimerEvenement' class='formType' style='display: none'>
-
-				<div class='buttonHideForm' onclick='hideForm(\"delConseil\")'>
-					<img src='./ressources/fermer_form.png' style='width: 30px; height: 30px;'>
-				</div>
-
-				<div class='policeTitre tailleTitre titreForm'>Supprimer Une Bulle</div><br>
-				
-				<form action='controleur.php' method='get'>
-
-					<select name='id' style='width: auto;'>";
-
-						foreach(listeEvenements() as $event){
-							echo "<option style='font-style:italic;' value='".$event['id']."'>".$event['id']." -- ".$event['titre']."</option>";
-						}
-		echo "
-					</select>
-
-					<input type='submit' class='buttonType' name='action' value='Supprimer Bulle'>
-				
-				</form>
-			</div>
-		";
-	}
-?>
