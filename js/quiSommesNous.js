99e82a5aa2cada4b1a065905f48b75d021feefc8
Martin Ducoulombier <martin.ducoulombier@free.fr>
2023-08-09 17:25:37 +0200

    patch

--- js/quiSommesNous.js
+++
@@ -1,6 +0,0 @@
-//addEventListener("scroll", scrollFunction);
-
-function scrollFunction() {
-    document.getElementById("quiSommesNous").children[0].style.opacity = Math.min(1, Math.max(1 - window.scrollY / 550, 0));
-    document.getElementById("quiSommesNous").children[1].style.opacity = Math.min(1, Math.max(1 - (window.scrollY-950) / 550, 0));
-}
